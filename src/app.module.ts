6c7886d7f46cbb2a214f74b0fdd32f974301f6ea
TomioDeCode <tomiodecode@gmail.com>
2024-09-17 10:48:20 +0700

    learn: basic middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs'); Options 1
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); Options 2
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // Options 3
+  }
+}
